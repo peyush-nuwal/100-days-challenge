86ddd3fa386f7ae3aeffa5f9e258013416d650ee
Peyush Nuwal <piyushnawal19@gmail.com>
2025-09-08 13:35:07 +0530

    first commit

---
+++ solve_expression.js
@@ -0,0 +1,12 @@
+
+// Question 1: solving a expression using eval()
+
+// input: "(3+2)*7/2-5"
+// output:
+
+
+const method1 = (exp) => {
+    return eval(exp)
+}
+
+console.log("output :",method1("(3+2)*7/2-5"))
