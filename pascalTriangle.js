3af03f36e55580f664a4de2d0880105913ed87ee
Peyush Nuwal <piyushnawal19@gmail.com>
2025-09-22 21:05:34 +0530

    leetcode - 119 : pacal triangle 2

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -22,3 +22,28 @@ return triangle
 }
 
 console.log(createPascalTriangle(5));
+
+
+
+const getPacalTriangleRow = (rowIndex) => {
+    if (rowIndex === 0) return [1];
+    let row = [];
+
+    for (let i = 1; i <= rowIndex; i++){
+        let newRow = []
+        newRow.push(1)
+        for (let j = 1; j < i; j++){
+            let element = row[j - 1] + row[j]
+            newRow.push(element)
+        }
+        newRow.push(1)
+        row=newRow
+    }
+
+    return row
+
+}
+
+console.log(getPacalTriangleRow(1))
+console.log(getPacalTriangleRow(2))
+console.log(getPacalTriangleRow(5))
