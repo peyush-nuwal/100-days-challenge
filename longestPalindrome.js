4a95ede7b4e67ee9c7bfac3ba681c8f2c5f06365
Peyush Nuwal <piyushnawal19@gmail.com>
2025-10-05 23:56:41 +0530

    implement longest palindrome substring function

---
+++ longestPalindrome.js
@@ -0,0 +1,28 @@
+const longestPalindrome = (s) => {
+  if (s.length < 1) return s;
+
+  let start = 0;
+  let end = 0;
+
+  function expendAroundEdges(left, right) {
+    while (left >= 0 && right < s.length && s[left]=== s[right]) {
+      left--
+      right++;
+    }
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expendAroundEdges(i, i);
+    let len2 = expendAroundEdges(i, i + 1);
+    let len = Math.max(len1, len2);
+
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+  return s.substring(start, end + 1);
+};
+
+console.log(longestPalindrome("abbbas"));
