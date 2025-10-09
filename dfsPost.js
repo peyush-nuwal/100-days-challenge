cf5562908883c0849026c8d743ff15f99f5b525f
Peyush Nuwal <piyushnawal19@gmail.com>
2025-10-09 14:51:16 +0530

    implement depth-first post-order traversal for binary tree

---
+++ dfsPost.js
@@ -0,0 +1,30 @@
+function treeNode(val) {
+  this.val = val ?? 0;
+  this.left = null;
+  this.right = null;
+}
+
+const root = new treeNode(1);
+
+root.left = new treeNode(2);
+root.left.left = new treeNode(4);
+root.left.right = new treeNode(5);
+
+root.right = new treeNode(3);
+root.right.left = new treeNode(6);
+root.right.right = new treeNode(7);
+
+// depth first postOrder
+const dfsPost = (root, res = []) => {
+  if (!root) return res;
+  let node = root;
+
+  if (node.left) dfsPost(node.left, res);
+    if (node.right) dfsPost(node.right, res);
+    res.push(node.val)
+    
+
+  return res;
+};
+
+console.log(dfsPost(root));
